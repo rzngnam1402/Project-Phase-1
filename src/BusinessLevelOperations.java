be949b044adb87409a2f93678e9f339f2aa4eb13
rzngnam1402 <legiangnam1402@gmail.com>
2024-06-16 09:36:47 +0700

    feat: finish project phase 1

---
+++ src/BusinessLevelOperations.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class BusinessLevelOperations {
+    private final String folderPath = "/Users/legiangnam/Documents/Fast Retailing/Phase1_Project/exampleFolder";
+    private  File folder = new File(folderPath);
+
+    private List<String> fileList = new ArrayList<>();
+
+    public void showAllFiles() {
+        if (folder.isDirectory()) {
+            File[] listOfFiles = folder.listFiles();
+            System.out.println("\nList of file in current directories: ");
+
+            assert listOfFiles != null;
+
+            for(File file : listOfFiles) {
+                if(file.isFile()) {
+                    fileList.add(file.getName());
+                }
+            }
+            Collections.sort(fileList);
+            fileList.forEach(file -> System.out.println("\t" + file));
+            System.out.println();
+        }
+    }
+
+    public void addFile() {
+        System.out.println("Enter the file which you want to add:");
+        Scanner scan = new Scanner(System.in);
+        String filename = scan.nextLine();
+        File file = new File("exampleFolder/" + filename);
+        try {
+            if(file.createNewFile()) {
+                System.out.println(filename + " file is added to the directory");
+                Desktop.getDesktop().edit(file);
+            }
+            else {
+                System.out.println("This file is already there");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteFile() {
+        System.out.println("Enter the file which you want to delete:");
+        Scanner scan = new Scanner(System.in);
+        String filename = scan.nextLine();
+
+        File file = new File("exampleFolder/"+ filename);
+        if(file.delete())
+            System.out.println(filename + " is deleted");
+        else
+            System.out.println("File Not Found");
+    }
+
+    public void searchFile() {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter the file name which you want to search:");
+        String fileName=scan.nextLine();
+        File[] listOfFiles = folder.listFiles();
+
+        int flag=0;
+        assert listOfFiles != null;
+        for (File file : listOfFiles) {
+            String name = file.getName();
+                if (name.equals(fileName)) {
+                    try {
+
+                        Scanner output = new Scanner(file);
+                        while (output.hasNextLine())
+                        {
+                            System.out.println(output.nextLine());
+                        }
+                        return;
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        }
+        System.out.println("File not found");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,62 @@
+
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        welcomeScreen();
+        menu();
+    }
+
+    public static void welcomeScreen() {
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Application Name : LockedMe.com");
+        System.out.println("Created by Le Giang Nam");
+        System.out.println("----------------------------------------------------------------");
+    }
+
+    public static void menu() {
+        Scanner sc = new Scanner(System.in);
+        BusinessLevelOperations Ops = new BusinessLevelOperations();
+
+        int option;
+        do {
+            System.out.println("Enter your choice which you want to select: \n");
+            System.out.println("\t1. Retrieve current filenames in ascending order \n");
+            System.out.println("\t2. Business-level operation menu \n");
+            System.out.println("\t3. Exit from the application \n");
+            System.out.print("Your choice: ");
+
+            option=sc.nextInt();
+            switch (option) {
+                case 1 -> Ops.showAllFiles();
+                case 2 -> {
+                    int ch;
+                    do {
+                        System.out.println("Enter your choice for Business level Operation");
+                        System.out.println("\t1. Add a file and its content to a directory");
+                        System.out.println("\t2. Delete a file from a directory");
+                        System.out.println("\t3. Search a file and show its content");
+                        System.out.println("\t4. Go back");
+                        System.out.print("Your choice: ");
+
+                        ch = sc.nextInt();
+                        switch (ch) {
+                            case 1 -> Ops.addFile();
+                            case 2 -> Ops.deleteFile();
+                            case 3 -> Ops.searchFile();
+                            case 4 -> {
+                                System.out.println("Exiting...");
+                                System.out.println("-------------------------------------------\n");
+                            }
+                            default -> System.out.println("Invalid Choice");
+                        }
+                    } while (ch != 4);
+                }
+                case 3 -> System.out.println("Exiting from the application...");
+                default -> System.out.println("Invalid choice");
+            }
+        } while (option != 3);
+    }
+}
